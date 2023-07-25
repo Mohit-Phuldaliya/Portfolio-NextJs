f4e7c833f54f62bf8def6a4748334a42b8f60346
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2023-07-26 03:41:53 +0530

    Transition Effect Done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="./public/images/profile/homepagepic2.png" />
+        <link rel="icon" href="/images/profile/MP.png" />
       </Head>
       <main
         className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
